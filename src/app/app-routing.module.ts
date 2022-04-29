04e38b6b883af15d91d8be8e372a337f9a3004f2
santiago <alquileresBeltran@outlook.es>
2022-04-29 19:40:42 -0300

    se creo componente de crear cuenta y se agregaron estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "CrearCuenta", component: CrearCuentaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
---
+++ src/app/crear-cuenta/crear-cuenta.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-cuenta',
+  templateUrl: './crear-cuenta.component.html',
+  styleUrls: ['./crear-cuenta.component.css']
+})
+export class CrearCuentaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+    
+
+
+
+
+  }
+
+}
