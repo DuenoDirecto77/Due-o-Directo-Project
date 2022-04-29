adae42c31a521508894c9888c1a3818bcc0d3ae7
santiago <alquileresBeltran@outlook.es>
2022-04-29 19:50:30 -0300

    se creo componente de iniciar sesion  y se agregaron estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component'
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
 
 const routes: Routes = [
-  {path: "CrearCuenta", component: CrearCuentaComponent}
+  {path: "CrearCuenta", component: CrearCuentaComponent},
+  {path: "iniciarSesion", component: IniciarSesionComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CrearCuentaComponent
+    CrearCuentaComponent,
+    IniciarSesionComponent
   ],
   imports: [
     BrowserModule,
